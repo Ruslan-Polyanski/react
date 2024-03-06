078611bae2a51104c2981000ab1b0c0b0a28e029
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-03-06 19:41:33 +0300

    feat: add part react_2

--- react_2/src/App.js
+++ react_2/src/App.js
@@ -2,33 +2,30 @@
 import styles from './App.module.css';
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({value}) {
 
-  function handleClick() {
-    setValue('X')
-  }
-
-  return <button className={styles.square} onClick={handleClick}>{value}</button>
+  return <button className={styles.square} onClick={onSquareClick}>{value}</button>
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className={styles['board-row']}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className={styles['board-row']}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className={styles['board-row']}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
