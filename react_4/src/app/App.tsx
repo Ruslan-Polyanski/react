c2e7f61f45a7d11f7487932da589b819070e77d3
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-07-22 22:41:58 +0300

    feat: add project

---
+++ react_4/src/app/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import style from './style.module.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className={style.header}></header>
+      <div>gggggggggggg</div>
+      <div></div>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
