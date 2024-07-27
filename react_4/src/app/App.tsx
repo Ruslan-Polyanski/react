d8b6df1ce592cec67c1a1ae9082eed27f75b3a91
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-07-27 22:41:48 +0300

    feat: add react app

--- react_4/src/app/App.tsx
+++ react_4/src/app/App.tsx
@@ -1,16 +1,75 @@
 
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 import style from './style.module.css';
+import Button from '../components/button/Button';
 
+interface ITodoItem {
+  task: string; 
+  done: boolean;
+  id: string;
+}
 
+type ITodoList = Array<ITodoItem>;
 
-function App() {
+const initialTodoList: ITodoList = [
+  {task: 'Task one', done: false, id: nanoid()},
+  {task: 'Task two', done: false, id: nanoid()},
+  {task: 'Task three', done: false, id: nanoid()},
+  {task: 'Task four', done: false, id: nanoid()},
+];
+
+enum ErrorMessage {
+  emptyMessage = 'You did not enter any characters'
+}
+
+const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<ITodoList>(initialTodoList);
+  const [message, setMessage] = useState<string>('');
+  const [isErrorMessage, setIsErrorMessage] = useState<boolean>(false);
+
+  const handleTextMessage: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const handleClickButton: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    if(message.trim()) {
+      const newTodoList = [...todoList, {
+        task: message, 
+        done: false,
+        id: nanoid()
+      }];
+      setIsErrorMessage(false)
+      setTodoList(newTodoList)
+      setMessage('')
+    } else {
+      setIsErrorMessage(true)
+    }
+  }
+
   return (
     <div className="App">
-      <header className={style.header}></header>
-      <div>gggggggggggg</div>
-      <div></div>
-      <footer></footer>
+
+      <header className={style.header}>ToDo List</header>
+
+      <form>
+        <label>
+          Добавить задачу
+          <input placeholder='Add task' type="text" value={message} onChange={handleTextMessage} />
+        </label>
+        <Button handleClickButton={handleClickButton}>Отправить</Button>
+        <span>{isErrorMessage ? ErrorMessage.emptyMessage : ''}</span>
+      </form>
+
+      <ul>
+        <span>My new task: {message}</span>
+        {todoList.map(item => {
+          return <li key={item.id}>{item.task}</li>
+        })}
+      </ul>
+
     </div>
   );
 }
