003e0909763b0723a344c4025017bea71bb06431
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-07-28 22:14:55 +0300

    feat: add task react

--- react_4/src/app/App.tsx
+++ react_4/src/app/App.tsx
@@ -4,6 +4,8 @@ import { nanoid } from 'nanoid';
 import './App.css';
 import style from './style.module.css';
 import Button from '../components/button/Button';
+import Header from '../components/header/Header';
+import FormAddTask from '../components/form/formAddTask/FormAddTask';
 
 interface ITodoItem {
   task: string; 
@@ -20,17 +22,25 @@ const initialTodoList: ITodoList = [
   {task: 'Task four', done: false, id: nanoid()},
 ];
 
-enum ErrorMessage {
-  emptyMessage = 'You did not enter any characters'
-}
-
 const App: React.FC = () => {
   const [todoList, setTodoList] = useState<ITodoList>(initialTodoList);
   const [message, setMessage] = useState<string>('');
   const [isErrorMessage, setIsErrorMessage] = useState<boolean>(false);
+  const [editId, setEditId] = useState<string[]>([]);
 
   const handleTextMessage: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     setMessage(event.target.value)
+  }
+
+  const handleUpdateTaskMessage = (event: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    const updatedTask = todoList.map(item => {
+      if(item.id !== id) {
+        return item;
+      } else {
+        return {...item, task: event.target.value}
+      }
+    })
+    setTodoList(updatedTask)
   }
 
   const handleClickButton: React.MouseEventHandler<HTMLButtonElement> = (event) => {
@@ -49,24 +59,65 @@ const App: React.FC = () => {
     }
   }
 
+  const handleChangeDoneTask: ((id: string, done: boolean) => void) = (id, done) => {
+    const newTodoList = todoList.map((item) => {
+      if(id !== item.id) return item;
+      return {...item, done}
+    })
+    setTodoList(newTodoList)
+  }
+
+  const handleDeleteClick = (id: string) => {
+    const newTodoList = todoList.filter((item) => {
+      if(item.id !== id) return true;
+      return false;
+    })
+    setTodoList(newTodoList)
+  }
+
+  const handleEditClick = (id: string) => {
+    const set = new Set([...editId, id])
+    setEditId([...set])
+  }
+
+  const handleSaveClick = (id: string) => {
+    const set = new Set([...editId]);
+    set.delete(id)
+    setEditId([...set])
+  }
+
   return (
     <div className="App">
 
-      <header className={style.header}>ToDo List</header>
+      <Header />
 
-      <form>
-        <label>
-          Добавить задачу
-          <input placeholder='Add task' type="text" value={message} onChange={handleTextMessage} />
-        </label>
-        <Button handleClickButton={handleClickButton}>Отправить</Button>
-        <span>{isErrorMessage ? ErrorMessage.emptyMessage : ''}</span>
-      </form>
+      <div className={style.formPositin}>
+        <FormAddTask message={message} 
+                    handleTextMessage={handleTextMessage} 
+                    handleClickButton={handleClickButton}
+                    isErrorMessage={isErrorMessage}
+                    />
+      </div>
 
-      <ul>
-        <span>My new task: {message}</span>
+      <div className={style.messageError}>
+          <div className={style.trMessageBase}>My new task</div>
+          <div className={style.trMessageText}>{message}</div>
+      </div>
+
+      <ul className={style.list}>
         {todoList.map(item => {
-          return <li key={item.id}>{item.task}</li>
+          return (
+            <li key={item.id}>
+              <input type='checkbox' checked={item.done} onChange={() => handleChangeDoneTask(item.id, !item.done)} />
+              <span className={style.textTask}>{item.task}</span>
+              {editId.includes(item.id) ? <input value={item.task} onChange={(event) => handleUpdateTaskMessage(event, item.id)} /> 
+                                        : null
+                                        }
+              <Button isDisabled={editId.includes(item.id)} handleClickButton={() => handleEditClick(item.id)}>Edit</Button>
+              <Button isDisabled={!editId.includes(item.id)} handleClickButton={() => handleSaveClick(item.id)}>Close</Button>
+              <Button handleClickButton={() => handleDeleteClick(item.id)}>Deleite</Button>
+            </li>
+          )
         })}
       </ul>
 
